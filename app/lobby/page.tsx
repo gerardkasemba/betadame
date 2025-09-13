fa1db00e8eeb3d1e78dc910f78d56f336ac2a334
Gerard Kasemba <gerardkasemba@gmail.com>
2025-09-13 10:31:59 -0400

    Update page.tsx

--- app/lobby/page.tsx
+++ app/lobby/page.tsx
@@ -9,7 +9,7 @@ export default async function Lobby() {
   const { games, error } = await getOpenGames();
 
   return (
-    <div className="mx-auto mx-w-4xl p-0">
+    <div className="mx-auto mx-w-4xl p-4 md:p-0">
       {/* <h1 className="text-3xl font-bold mb-4">Lobby des parties</h1> */}
       {error && <p className="text-[#CE1126] mb-4 p-0 rounded bg-red-100">Erreur : {error.message}</p>}
       <GameLobby games={games || []} />
